9d6058caae07539dac01313afe98ed34d7b7e7d7
Everton <everton.gs.br1@gmail.com>
2023-07-07 20:19:22 -0300

    Implementação do pacote infrasecurety e classe de configuração da mesma

---
+++ src/main/java/com/example/Projectwebapi/infraSecurety/SecuretyConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.Projectwebapi.infraSecurety;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecuretyConfiguration {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+        .csrf(csrf->csrf.disable())
+        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
+        
+    }
+}
